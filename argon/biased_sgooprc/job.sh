11fd50ed4336373c82459ee856d96100a6e83084
Sun-Ting Tsai <lina492375qw1188@Sun-Tings-MacBook-Air.local>
2023-12-06 20:06:49 -0500

    add dir

---
+++ argon/biased_sgooprc/job.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name="argon"
+#SBATCH -p RM
+#SBATCH -t 00:10:00
+#SBATCH -N 4
+#SBATCH --ntasks-per-node 20
+
+module load plumed
+module load gcc
+module load gromacs/2020.2-cpu
+
+sh createTpr.sh
+mpirun -np $SLURM_NPROCS gmx_mpi mdrun -v -deffnm md -plumed plumed.dat
+
